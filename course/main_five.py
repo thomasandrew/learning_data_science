d5d3507c812ecd28782ca8770fbaef2aae341ea2
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-01-05 20:13:16 -0500

    Create main_five.py

---
+++ course/main_five.py
@@ -0,0 +1,27 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's anothe state's name: ").title()
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        #t.write(state_data.state)
+        #t.write(state_data.state.item())
+        t.write(answer_state)
+
+screen.exitonclick()
